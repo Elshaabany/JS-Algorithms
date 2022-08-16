f4ed68cfd5b0bea79ada72fba0949bc2330a754d
Eslam Elshaabany <Elshaabany@protonmail.com>
2022-08-16 23:16:47 +0200

    making it pure

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -1,10 +1,13 @@
-const bubbleSort = (arr) => {
-    for (let i = 0; i < arr.length; i++)
-        for (let j = 0; j < arr.length - i; j++)
+const bubbleSort = (array) => {
+    let arr = [...array];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
+        }
+    }
     return arr;
 }
